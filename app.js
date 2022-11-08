f3d8708de878024622b5c40261bb87542ea4bcba
Mariano <marianolangge@gmail.com>
2022-11-08 15:54:34 -0300

    carrito

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 
-  //MODAL-CARRITO
-  const openModal = document.getElementById("open-modal")
-  const closeModal = document.getElementById("modal-cerrar")
-  const modalContain = document.getElementById("modal-container")
+//MODAL-CARRITO
+const openModal = document.querySelector("#open-modal")
+const closeModal = document.querySelector("#modal-close")
+const modalContain = document.querySelector("#modal-container")
 
-  openModal.addEventListener("click", () => {
-    modalContain.classList.add("modal-container-active")
-    console.log("funciona el boton")
-    
-  })
-  
-  closeModal.addEventListener("click", () => {
-    modalContain.classList.remove("modal-container-active")
-    console.log("funciona el boton d cierre")
-  })
+openModal.addEventListener("click", () => {
+  modalContain.classList.add("modal-container-active")
+  console.log("funciona el boton")
+
+})
+
+closeModal.addEventListener("click", () => {
+  modalContain.classList.remove("modal-container-active")
+  console.log("funciona el boton d cierre")
+})
 
 
 //PRODUCT-LIST
 
-function Item(item){
-  
-    return `<div class="product-box">
+function Item(item) {
+
+  return `<div class="product-box">
       <div class="img-contain">
         <img class="img-product" src="${item.image}" alt="">
       </div>
@@ -29,81 +29,81 @@ function Item(item){
         <h4 class="price">$${item.price}</h4>
       </div>
       <div class="info">
-        <spam>6 x $${item.dues}, sin interes.</spam>
+        <span class="dues">6 x $${item.dues}, sin interes.</span>
         <br>
         <br>
-        <spam>${item.description}</spam>
+        <span class="description">${item.description}</span>
       </div>
       <div class="btn">
         <buttom id="add">Sumar</buttom>
     </div>
   </div>`
-  }
-  
-  function addItem(item){
-    document.getElementById("products-contain").innerHTML += Item(item);
-  }
-  
-  const items = [
-    {
-    id: 1,
-    image : "https://http2.mlstatic.com/D_Q_NP_667712-MLA46445221923_062021-AB.webp",
-    title : "Kit teclado y mous 1",
-    price :  9599,
-    dues: 1599.83,
-    description:"Kit de teclado y mouse gamer Razer Cynosa Lite + Abyssus Lite Español de color negro"
-  },
-    {
-    id: 2,  
-    image : "https://http2.mlstatic.com/D_Q_NP_667712-MLA46445221923_062021-AB.webp",
-    title : "Kit Gamer Tedge Teclado + Mouse + Auriculares Color Negro",
-    price : 9599,
-    dues: 1599.83,
-    description:"Kit de teclado y mouse gamer Razer Cynosa Lite + Abyssus Lite Español de color negro"
-  },
-    {
-    id: 3,
-    image : "https://http2.mlstatic.com/D_Q_NP_667712-MLA46445221923_062021-AB.webp",
-    title : "Kit teclado y mous 2",
-    price : 9599,
-    dues: 1599.83,
-    description:"Kit de teclado y mouse gamer Razer Cynosa Lite + Abyssus Lite Español de color negro"
-  },
-    {
-    id: 4,
-    image : "https://http2.mlstatic.com/D_Q_NP_667712-MLA46445221923_062021-AB.webp",
-    title : "Kit teclado y mous 2",
-    price : 9599,
-    dues: 1599.83,
-    description:"Kit de teclado y mouse gamer Razer Cynosa Lite + Abyssus Lite Español de color negro"
-  },
-    {
-    id: 5,
-    image : "https://http2.mlstatic.com/D_Q_NP_667712-MLA46445221923_062021-AB.webp",
-    title : "Kit teclado y mous 2",
-    price : 9599,
-    dues: 1599.83,
-    description:"Kit de teclado y mouse gamer Razer Cynosa Lite + Abyssus Lite Español de color negro"
-  },
-    {
-    id: 6,
-    image : "https://http2.mlstatic.com/D_Q_NP_667712-MLA46445221923_062021-AB.webp",
-    title : "Kit teclado y mous 2",
-    price : 9599,
-    dues: 1599.83,
-    description:"Kit de teclado y mouse gamer Razer Cynosa Lite + Abyssus Lite Español de color negro"
-  },
-  ]
-  
-  items.forEach((item)=>{
-    addItem(item)
-  })
-  
-  //PRODUCT-FILTER
+}
 
-  const searchBtn = document.getElementById("search-button")
-  searchBtn.addEventListener("click", () => {
-    const searchInput = document.getElementById("search-input").value
-    
-    console.log(searchInput)
-  })
+function addItem(item) {
+  document.getElementById("products-contain").innerHTML += Item(item);
+}
+
+const items = [
+  {
+    id: 1,
+    image: "https://http2.mlstatic.com/D_Q_NP_667712-MLA46445221923_062021-AB.webp",
+    title: "Kit teclado y mous 1",
+    price: 9599,
+    dues: 1599.83,
+    description: "Kit de teclado y mouse gamer Razer Cynosa Lite + Abyssus Lite Español de color negro"
+  },
+  {
+    id: 2,
+    image: "https://http2.mlstatic.com/D_Q_NP_667712-MLA46445221923_062021-AB.webp",
+    title: "Kit Gamer Tedge Teclado + Mouse + Auriculares Color Negro",
+    price: 9599,
+    dues: 1599.83,
+    description: "Kit de teclado y mouse gamer Razer Cynosa Lite + Abyssus Lite Español de color negro"
+  },
+  {
+    id: 3,
+    image: "https://http2.mlstatic.com/D_Q_NP_667712-MLA46445221923_062021-AB.webp",
+    title: "Kit teclado y mous 2",
+    price: 9599,
+    dues: 1599.83,
+    description: "Kit de teclado y mouse gamer Razer Cynosa Lite + Abyssus Lite Español de color negro"
+  },
+  {
+    id: 4,
+    image: "https://http2.mlstatic.com/D_Q_NP_667712-MLA46445221923_062021-AB.webp",
+    title: "Kit teclado y mous 2",
+    price: 9599,
+    dues: 1599.83,
+    description: "Kit de teclado y mouse gamer Razer Cynosa Lite + Abyssus Lite Español de color negro"
+  },
+  {
+    id: 5,
+    image: "https://http2.mlstatic.com/D_Q_NP_667712-MLA46445221923_062021-AB.webp",
+    title: "Kit teclado y mous 2",
+    price: 9599,
+    dues: 1599.83,
+    description: "Kit de teclado y mouse gamer Razer Cynosa Lite + Abyssus Lite Español de color negro"
+  },
+  {
+    id: 6,
+    image: "https://http2.mlstatic.com/D_Q_NP_667712-MLA46445221923_062021-AB.webp",
+    title: "Kit teclado y mous 2",
+    price: 9599,
+    dues: 1599.83,
+    description: "Kit de teclado y mouse gamer Razer Cynosa Lite + Abyssus Lite Español de color negro"
+  },
+]
+
+items.forEach((item) => {
+  addItem(item)
+})
+
+//PRODUCT-FILTER
+
+const searchBtn = document.getElementById("search-button")
+searchBtn.addEventListener("click", () => {
+  const searchInput = document.getElementById("search-input").value
+
+  console.log(searchInput)
+})
